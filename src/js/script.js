098192c546eca23f0973e731bbef2b97f8dbd0e1
Mihir-Raj-Singh <mihirrajsingh123@gmail.com>
2025-06-25 09:38:54 +0530

    Added Sliding Companies Section
    - [x] Implemented sliding companies section
    - [x] Change body background to bottom from bottom-right
    - [x] Added new assets for company logos

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,48 @@
 const navDialog = document.querySelector("#nav-dialog");
 
+// Hamburger Menu Handler
 function handleMenu() {
   navDialog.classList.toggle("hidden");
 }
+
+// Companies Lists Scroll Handler
+
+const initialTranslateLTR = -48 * 4;
+const initialTranslateRTL = 36 * 4;
+
+function setupIntersectionObserver(element, isLTR, speed) {
+  const intersectionCallback = (entries) => {
+    const isIntersecting = entries[0].isIntersecting;
+    console.log(element, isIntersecting);
+    if (isIntersecting) {
+      document.addEventListener("scroll", scrollHandler);
+    } else {
+      document.removeEventListener("scroll", scrollHandler);
+    }
+  };
+
+  const intersectionObserver = new IntersectionObserver(intersectionCallback);
+  intersectionObserver.observe(element);
+
+  function scrollHandler() {
+    const translateX =
+      (window.innerHeight - element.getBoundingClientRect().top) * speed;
+
+    let totalTranslate = 0;
+    if (isLTR) {
+      totalTranslate = translateX + initialTranslateLTR;
+    } else {
+      totalTranslate = -(translateX + initialTranslateRTL);
+    }
+
+    element.style.transform = `translateX(${totalTranslate}px)`;
+  }
+}
+
+const line1 = document.querySelector("#line1");
+const line2 = document.querySelector("#line2");
+const line3 = document.querySelector("#line3");
+
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
